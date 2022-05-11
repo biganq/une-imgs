c9d885c79bb2917861d706b4c72bc1b7754ade1c
xbox366 <54818355@qq.com>
2022-05-11 19:50:00 +0800

    741

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Inter':['sans-serif']
+      }
+    },
   },
   plugins: [],
 }
